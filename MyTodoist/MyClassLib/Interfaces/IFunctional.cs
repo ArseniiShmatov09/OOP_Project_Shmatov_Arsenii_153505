45139ac9400fdf92decc94ebc934f5265ed206ad
ArseniiShmatov09 <shmatovarsenii09@gmail.com>
2023-04-20 17:42:10 +0300

    added Interfaces

---
+++ MyTodoist/MyClassLib/Interfaces/IFunctional.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MyClassLib.Interfaces
+{
+    public interface IFunctional<T>
+    {
+        void Add(string name, string description);
+        void Remove(T task);
+        void Change(T task, string newName, string newDescription);
+        T Find(string name);
+        List<T> GetList();
+    }
+}
---
+++ MyTodoist/MyClassLib/Interfaces/IFunctionalController.cs
@@ -0,0 +1,11 @@
+﻿namespace MyClassLib.Interfaces
+{
+    public interface IFunctionalController
+    {
+        void Add(string name, string description);
+        void Remove(string name);
+        void Change(string name, string newName, string newDescription);
+        public void Show();
+
+    }
+}
