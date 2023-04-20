d9ddf6ec8c2bc33315de87ad7dd6cd0fc4e48437
ArseniiShmatov09 <shmatovarsenii09@gmail.com>
2023-04-20 17:41:20 +0300

    added Containers

---
+++ MyTodoist/MyClassLib/Containers/ProjectContainer.cs
@@ -0,0 +1,86 @@
+﻿using MyClassLib.Interfaces;
+using MyClassLib.Models;
+
+namespace MyClassLib.Containers
+{
+    public class ProjectContainer : IFunctional<Project>
+    {
+        private List<Project> Projects = new List<Project>();
+        private Project Others = new Project("Другие", "Для различных задач");
+
+        public ProjectContainer()
+        {
+            Add(Others.Name, Others.Description);
+        }
+
+        public void Add(string name, string description)
+        {
+            Projects.Add(new Project(name, description));
+        }
+
+        public void Remove(Project project)
+        {
+            Projects.Remove(project);
+        }
+
+        public void Change(Project project, string newName, string newDescription)
+        {
+            project.Name = newName;
+            project.Description = newDescription;
+        }
+
+        public Project? Find(string name)
+        {
+            return Projects.Find(x => x.Name == name);
+        }
+
+        public List<Project> GetList()
+        {
+            return Projects;
+        }
+
+        public void AddTask(string taskName, string taskDescription, string projectName = "Другие")
+        {
+            Project? item = Projects.Find(x => x.Name == projectName);
+            item?.AddTask(taskName, taskDescription);
+        }
+
+        public void RemoveTask(Models.Task task, string projectName = "Другие")
+        {
+            Project? item = Projects.Find(x => x.Name == projectName);
+            item?.RemoveTask(task);
+        }
+
+        public void ChangeTask(Models.Task task, string newName, string newDescription, string projectName = "Другие")
+        {
+            Project? item = Projects.Find(x => x.Name == projectName);
+            item?.ChangeTask(task, newName, newDescription);
+        }
+
+        public Models.Task? FindTask(string taskName, string projectName = "Другие")
+        {
+            Project? item = Projects.Find(x => x.Name == projectName);
+            return item?.FindTask(taskName);
+        }
+
+        public List<Models.Task>? GetTasksList(string projectName = "Другие")
+        {
+            Project? item = Projects.Find(x => x.Name == projectName);
+            return item?.GetTasks();
+        }
+
+        public void CompleteTask(Models.Task task, string projectName = "Другие")
+        {
+            Project? item = Projects.Find(x => x.Name == projectName);
+            item?.CompleteTask(task);
+        }
+
+        public List<Models.Task>? GetCompletedTasksList(string projectName = "Другие")
+        {
+            Project? item = Projects.Find(x => x.Name == projectName);
+            return item?.GetCompletedTasks();
+        }
+
+
+    }
+}
---
+++ MyTodoist/MyClassLib/Containers/TaskContainer.cs
@@ -0,0 +1,50 @@
+﻿using MyClassLib.Models;
+using MyClassLib.Interfaces;
+
+namespace MyClassLib.Containers
+{
+    public class TaskContainer : IFunctional<Models.Task>
+    {
+        private List<Models.Task> Tasks = new List<Models.Task>();
+        private List<Models.Task> CompletedTasks = new List<Models.Task>();
+
+        public void Add(string name, string description)
+        {
+            Tasks.Add(new Models.Task(name, description));
+        }
+
+        public void Remove(Models.Task task)
+        {
+            Tasks.Remove(task);
+        }
+
+        public void Change(Models.Task task, string newName, string newDescription)
+        {
+            task.Name = newName;
+            task.Description = newDescription;
+        }
+
+        public Models.Task? Find(string name)
+        {
+            return Tasks.Find(x => x.Name == name);
+        }
+
+        public List<Models.Task> GetList()
+        {
+            return Tasks;
+        }
+
+        public void Complete(Models.Task task)
+        {
+            task.IsCompleted = true;
+            Tasks.Remove(task);
+            CompletedTasks.Add(task);
+        }
+
+        public List<Models.Task> GetCompletedList()
+        {
+            return CompletedTasks;
+        }
+
+    }
+}
